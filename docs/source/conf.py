8d01bd56d2770dbfc643b0037b9428f6f22b8a9f
Michael Pattison <36170808+mikey-p-codes@users.noreply.github.com>
2025-01-13 09:59:56 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,19 +10,24 @@ version = '0.1.0'
 # -- General configuration ---------------------------------------------------
 
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
-# -- Options for HTML output -------------------------------------------------
+templates_path = ['_templates']
+
+# -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
-# -- Options for todo extension ----------------------------------------------
-
-todo_include_todos = True
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
