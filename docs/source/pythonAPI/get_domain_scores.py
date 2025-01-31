eb81541a0fbd0747a4917174799cb3632c30fd61
Michael Pattison <36170808+mikey-p-codes@users.noreply.github.com>
2025-01-31 17:28:46 -0500

    Update get_domain_scores.py
    updated script and start and end date examples

--- docs/source/pythonAPI/get_domain_scores.py
+++ docs/source/pythonAPI/get_domain_scores.py
@@ -7,13 +7,13 @@ Time is represented as linux epoch time.
 
 Note: The Linux epoch time for Jan 1, 1970, is 0.
 Today's date in Linux Epoch time is: 1736804009 (for example, if today is January 13 2025)
-It appears that if you attempt to go too far back you will get an internal server error'''
+You can also use ISO 8601 which is provided as an example in the script'''
 
 def get_domain_scores():
     url = BASEURL + "process/domains"
     headers = { "Authorization": f"Bearer {TOKEN}" }
-    params = { "s": 1736800000, 
-               "e": 1736890409 
+    params = { "s": "2024-08-1T01:00:00", 
+               "e": "2025-01-31T01:00:00"
               }
     response = requests.get(url, headers=headers, params=params)
     if response.status_code == 200:
